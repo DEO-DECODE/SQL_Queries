0a6abdad8656164ae6832394620b152a1987fc09
Deo Raj <d.jrajsingh81@gmail.com>
2024-01-29 18:07:51 +0530

    Number Of Unique Subjects Taught by a teacher

---
+++ NumberOfUniqueSubjects.sql
@@ -0,0 +1,31 @@
+Table: Teacher
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| teacher_id  | int  |
+| subject_id  | int  |
+| dept_id     | int  |
++-------------+------+
+(subject_id, dept_id) is the primary key (combinations of columns with unique values) of this table.
+Each row in this table indicates that the teacher with teacher_id teaches the subject subject_id in the department dept_id.
+ 
+
+Write a solution to calculate the number of unique subjects each teacher teaches in the university.
+
+Return the result table in any order.
+
+The result format is shown in the following example.
+
+Create table If Not Exists Teacher (teacher_id int, subject_id int, dept_id int)
+Truncate table Teacher
+insert into Teacher (teacher_id, subject_id, dept_id) values ('1', '2', '3')
+insert into Teacher (teacher_id, subject_id, dept_id) values ('1', '2', '4')
+insert into Teacher (teacher_id, subject_id, dept_id) values ('1', '3', '3')
+insert into Teacher (teacher_id, subject_id, dept_id) values ('2', '1', '1')
+insert into Teacher (teacher_id, subject_id, dept_id) values ('2', '2', '1')
+insert into Teacher (teacher_id, subject_id, dept_id) values ('2', '3', '1')
+insert into Teacher (teacher_id, subject_id, dept_id) values ('2', '4', '1')
+
+
+select teacher_id , count(distinct(subject_id)) as cnt from Teacher group by teacher_id;
