489e773768b6e39c1a4d4da51bf32a571e24619d
Deo Raj <d.jrajsingh81@gmail.com>
2024-02-02 00:20:40 +0530

    Customers Who Bought All Products

---
+++ CustomersWhoBoughtAllProducts.sql
@@ -0,0 +1,45 @@
+Table: Customer
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| customer_id | int     |
+| product_key | int     |
++-------------+---------+
+This table may contain duplicates rows. 
+customer_id is not NULL.
+product_key is a foreign key (reference column) to Product table.
+ 
+
+Table: Product
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| product_key | int     |
++-------------+---------+
+product_key is the primary key (column with unique values) for this table.
+ 
+
+Write a solution to report the customer ids from the Customer table that bought all the products in the Product table.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+Create table If Not Exists Customer (customer_id int, product_key int)
+Create table Product (product_key int)
+Truncate table Customer
+insert into Customer (customer_id, product_key) values ('1', '5')
+insert into Customer (customer_id, product_key) values ('2', '6')
+insert into Customer (customer_id, product_key) values ('3', '5')
+insert into Customer (customer_id, product_key) values ('3', '6')
+insert into Customer (customer_id, product_key) values ('1', '6')
+Truncate table Product
+insert into Product (product_key) values ('5')
+insert into Product (product_key) values ('6')
+
+
+select customer_id from Customer group by customer_id having count(distinct(product_key))=
+(select count(product_key) from Product)
