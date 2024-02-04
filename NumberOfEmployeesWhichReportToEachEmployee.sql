ca36c1a127f4504dcc49935cb393729857f99883
Deo Raj <d.jrajsingh81@gmail.com>
2024-02-04 16:37:22 +0530

    The Number of Employees Which Report to Each Employee

---
+++ NumberOfEmployeesWhichReportToEachEmployee.sql
@@ -0,0 +1,34 @@
+Table: Employees
+
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| employee_id | int      |
+| name        | varchar  |
+| reports_to  | int      |
+| age         | int      |
++-------------+----------+
+employee_id is the column with unique values for this table.
+This table contains information about the employees and the id of the manager they report to. Some employees do not report to anyone (reports_to is null). 
+ 
+
+For this problem, we will consider a manager an employee who has at least 1 other employee reporting to them.
+
+Write a solution to report the ids and the names of all managers, the number of employees who report directly to them, and the average age of the reports rounded to the nearest integer.
+
+Return the result table ordered by employee_id.
+
+The result format is in the following example.
+
+
+Create table If Not Exists Employees(employee_id int, name varchar(20), reports_to int, age int)
+Truncate table Employees
+insert into Employees (employee_id, name, reports_to, age) values ('9', 'Hercy', 'None', '43')
+insert into Employees (employee_id, name, reports_to, age) values ('6', 'Alice', '9', '41')
+insert into Employees (employee_id, name, reports_to, age) values ('4', 'Bob', '9', '36')
+insert into Employees (employee_id, name, reports_to, age) values ('2', 'Winston', 'None', '37')
+
+
+select emp.employee_id, emp.name, temp.cnt as reports_count, temp.average_age from
+(select reports_to, count(reports_to) as cnt, round(avg(age)) as average_age from Employees group by reports_to having
+count(reports_to)>=1) as temp inner join Employees as emp on emp.employee_id= temp.reports_to order by emp.employee_id
