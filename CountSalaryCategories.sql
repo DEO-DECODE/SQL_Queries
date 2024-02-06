15ff525bacd2d1149e8fbb03e414e4ef8509e6cc
Deo Raj <d.jrajsingh81@gmail.com>
2024-02-06 20:53:42 +0530

    Count Salary Categories

---
+++ CountSalaryCategories.sql
@@ -0,0 +1,40 @@
+Table: Accounts
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| account_id  | int  |
+| income      | int  |
++-------------+------+
+account_id is the primary key (column with unique values) for this table.
+Each row contains information about the monthly income for one bank account.
+ 
+
+Write a solution to calculate the number of bank accounts for each salary category. The salary categories are:
+
+"Low Salary": All the salaries strictly less than $20000.
+"Average Salary": All the salaries in the inclusive range [$20000, $50000].
+"High Salary": All the salaries strictly greater than $50000.
+The result table must contain all three categories. If there are no accounts in a category, return 0.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+Create table If Not Exists Accounts (account_id int, income int)
+Truncate table Accounts
+insert into Accounts (account_id, income) values ('3', '108939')
+insert into Accounts (account_id, income) values ('2', '12747')
+insert into Accounts (account_id, income) values ('8', '87709')
+insert into Accounts (account_id, income) values ('6', '91796')
+
+
+select "Low Salary" as category, count(account_id) as accounts_count 
+from Accounts where income<20000
+union
+select "Average Salary" as category, count(account_id) as accounts_count 
+from Accounts where income between 20000 and 50000
+union
+select "High Salary" as category, count(account_id) as accounts_count 
+from Accounts where income >50000
